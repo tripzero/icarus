950fed19b8902805611960e762e90008cc9aa538
Ryan Kapur <ryankapur@berkeley.edu>
2015-06-12 16:58:01 -0700

    Added first-draft functionality for second actuator. 	modified:   actuator_pwm.py 	modified:   config.json 	modified:   pwm_funcs.py 	deleted:    tracker.py 	modified:   tracker_funcs.py

--- python/tracking.py
+++ python/tracking.py
@@ -16,13 +16,18 @@ print("Local time: ", curr_time)
 print()
 jf1 = t.Location("JF1", 45.541718, -122.960381)
 t.printLocationInfo(jf1)
-t.calcActuatorHeight(jf1, 2.8)
+t.calcTiltingHeight(jf1, 2.8)
+t.calcPanningHeight(jf1, 2.8)
 print()
 
 #From config.json
 print("Current location via config.json:")
 myLoc = t.Location( config["locationInfo"]["name"], config["locationInfo"]["latitude"], config["locationInfo"]["longitude"])
-distActuatorToOrigin = config["distInfo"]["distActuatorToOrigin"]
+distTiltToOrigin = config["distInfo"]["distActuatorToOrigin"]
 t.printLocationInfo(myLoc)
-inches = t.calcActuatorHeight(myLoc, distActuatorToOrigin)
-effectiveActuatorHeight = inches
+inches = t.calcTiltingHeight(myLoc, distTiltToOrigin)
+effectiveActuatorHeight1 = inches
+
+#calculating second actuator (panning)
+distPanToOrigin = config["distInfo"]["distPanningActuatorToOrigin"]
+effectiveActuatorHeight2 = t.calcPanningHeight(myLoc, distPanToOrigin)
