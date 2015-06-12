17b5c8cdd63bd48f104cc08c8ec38495613977c9
Ryan Kapur <ryankapur@berkeley.edu>
2015-06-12 16:58:01 -0700

    Added first-draft functionality for second actuator. 	modified:   actuator_pwm.py 	modified:   config.json 	modified:   pwm_funcs.py 	deleted:    tracker.py 	modified:   tracker_funcs.py

--- python/tracker_funcs.py
+++ python/tracker_funcs.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # import pysolar #docs.pysolar.org/en/latest/
 import datetime, json
-from pysolar.solar import *
+from pysolar.solar import * #TODO: potentially inefficent?
 from pprint import pprint
-from math import tan, radians
+from math import tan, cos, radians
 
 curr_time = datetime.datetime.now()
 
@@ -20,6 +20,8 @@ class Location:
 
 	def alt(self, lat, lon):
 		a = get_altitude(lat, lon, curr_time)
+		if a < 0:
+			raise ValueError("Cannot calculate. Sun is below the horizon.")
 		return a
 
 	def azimuth(self, lat, lon):
@@ -27,7 +29,7 @@ class Location:
 		return a
 
 
-"""Print all relevant location Data"""
+"""Print all relevant location data"""
 def printLocationInfo(loc):
 	print("location object name: ", loc.name)
 	print_coords(loc)
@@ -41,7 +43,7 @@ def print_alt(loc):
 def print_azimuth(loc):
 	print(str(loc.name) + " azimuth: ", loc.azimuth(loc.lat, loc.lon))
 
-# a = Actuator height
+# a = Actuator height #Note-first, panning actuator calculations
 # o = origin
 # H = distance from actuator to the pivot socket = distActuatorToPivot
 #         /|  |
@@ -56,16 +58,28 @@ def print_azimuth(loc):
 #O<-pivot bolt
 # ---------<-distActuatorToOrigin
 
-"""Calculate the most effective height of the actuator based upon the assumption solar panels are most efficent angled 45 degrees to the sun. Dist(a, o) is represented by base. Math: tan(S1) = a / distActuatorToOrigin & S1 = 90 - altitude ==> a = tan(90-altitude) * distActuatorToOrigin"""
-def calcActuatorHeight(loc, actuatorOriginDist):
+"""Calculate the most effective height of the first tilting actuator based upon the assumption solar panels are most efficent angled 45 degrees to the sun. Dist(a, o) is represented by base. Math: tan(S1) = a / distActuatorToOrigin & S1 = 90 - altitude ==> a = tan(90-altitude) * distActuatorToOrigin"""
+def calcTiltingHeight(loc, actuatorOriginDist):
 	val = 90 - loc.alt(loc.lat, loc.lon)
 	left = tan(radians(val))
 	right = actuatorOriginDist
 	a = left * right
-	print("Calculated (effective) actuator height: ", a, " inches")
+	print("Calculated, effective actuator height: ", a, " inches")
 	return a
 
+#######################################################################################################################
+#New stuff 
 
-
-
+"""Return the value calculated via the law of consines, the # of inches the second actuator must be move in order to pan the solar panel according to the azimuth."""
+def calcPanningHeight(loc, distPanningToOrigin):
+	azimuth = loc.azimuth(loc.lat, loc.lon)
+	azimuth = -azimuth
+	# if azimuth < 0:
+	# 	azimuth = 360 + azimuth
+	val = 2*distPanningToOrigin*distPanningToOrigin - (2*distPanningToOrigin*distPanningToOrigin*cos((radians(azimuth))))
+	x = math.sqrt(val)
+	print("value to sqrt: ", val)
+	print("Effective panning height(x): ", x)
+	print("azimuth: ", azimuth, " ", radians(azimuth))
+	return x
 
