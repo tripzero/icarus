45f49af379deaa365447e01f6d863c12ae652083
Ryan Kapur <ryankapur@berkeley.edu>
2015-06-15 14:15:41 -0700

    Modularized code; working on datetime issue; func demoDay. Actuator vals are malformed currently for altitude due to improper datetime. Must convert UTC<-> Pacific Time with an online module hopefully by tomorrow morning.
    	modified:   actuator_pwm.py
    	modified:   config.json
    	modified:   pwm_funcs.py
    	modified:   tracker_funcs.py
    	modified:   tracking.py

--- python/pwm_funcs.py
+++ python/pwm_funcs.py
@@ -2,25 +2,26 @@
 #Documentation:  http://iotdk.intel.com/docs/master/mraa/python/mraa.html#mraa.Pwm
 import mraa, time
 
+
 class Actuator:
 	#isPWM is true iff Pwm module is utilized	
 	def __init__(self, pin, maxHeight, percent, period, isPWM):
 		self.pin = pin
+		self.isPWM = isPWM
+		if isPWM:
+			x = mraa.Pwm(pin)
 		self.maxHeight = maxHeight
 		self.percent = percent
 		self.period = period
-		self.isPWM = isPWM
-		if isPWM:
-			self.module = mraa.Pwm(pin)
 
 	def set(self, percent):
 		if self.isPWM:
-			self.module.enable(True)
-			self.module.period_us(700)
+			x.enable(True)
+			x.period_us(700)
 
 			val = 0
 			while val <= percent:
 				print(val)
-				self.module.write(val)
+				x.write(val)
 				time.sleep(0.09)
 				val = val + 0.01
