fac29d2b07b7d3c10dba33636784e2f9adfd653f
Ryan Kapur <ryankapur@berkeley.edu>
2015-06-16 14:25:20 -0700

    -Created seperate demo.py file. Simplified datetime syntax. -Have not implemented time zone or dawn/dusk API yet. -Modified calculations taking into account roof slope

--- python/tracker_funcs.py
+++ python/tracker_funcs.py
@@ -6,9 +6,13 @@ from pprint import pprint
 from pysolar.time import get_delta_t, tt_offset, get_leap_seconds
 from math import tan, cos, radians
 
-t = datetime.datetime(2015, 6, 15, 23, 00, 00, tzinfo = datetime.timezone.utc)
+#Initialize inputs
+with open('config.json') as dataFile:
+	config = json.load(dataFile)
+# print("config.json: ")
+# pprint(config)
 
-original = datetime.datetime.now()
+now = datetime.datetime.now()
 """A location has time-specific altitude, azimuth values (degrees) as per Pysolar. Using origin to actuator distances, the various effective actuator heights can be calculated."""
 class Location:
 	"""Returns a new Location object"""
@@ -22,26 +26,18 @@ class Location:
 		original = time
 
 	def incrementTime(self, time):
-		self.time += datetime.timedelta(minutes= get_delta_t(self.time) + tt_offset - get_leap_seconds(self.time))
-		# print("get_delta_t(self.time): ", get_delta_t(self.time))
-		# print( ", tt_offset: ", tt_offset)
-		# print( " get_leap_seconds(self.time): ", get_leap_seconds(self.time))
-		
-
-		##time.tt_offset - time.get_leap_seconds(self.d))
-		#t += datetime.timedelta(hours= time.get_delta_t(self.time) + timeOffset)
-		#self.time = self.time.now()
+		self.time += datetime.timedelta(minutes = 60)
 
 	def resetTime(self, time):
-		time = original
+		time = now
 
 	def coords(self):
 		return (self.lat, self.lon)
 
 	def alt(self, lat, lon, time):
 		a = get_altitude(lat, lon, time)
-		# if a < 0:
-		# 	raise ValueError("Cannot calculate. Sun is below the horizon.")
+		if a < 0:
+			raise ValueError("Cannot calculate. Sun is below the horizon.")
 		return a
 
 	def azimuth(self, lat, lon, time):
@@ -63,49 +59,44 @@ class Location:
 	#O<-pivot bolt
 	# ---------<-distActuatorToOrigin
 
-	"""Calculate the most effective height of the first tilting actuator based upon the assumption solar panels are most efficent angled 45 degrees to the sun. Dist(a, o) is represented by base. Math: tan(S1) = a / distActuatorToOrigin & S1 = 90 - altitude ==> a = tan(90-altitude) * distActuatorToOrigin"""
+	"""Calculate the most effective height of the first tilting actuator based upon the assumption solar panels are most efficent angled 45 degrees to the sun. Dist(a, o) is represented by base. Math: tan(S1) = a / distActuatorToOrigin & S1 = 90 - altitude ==> a = tan(90-altitude) * distActuatorToOrigin.  Note: the house is angled at 21 degrees so we must take the tangent of (61-altitude) in actuality...a = tan(61-alt) *distActuatorToOrigin"""
 	def calcTiltingHeight(self, o_a_dist1):
-		val = 90 - self.alt(self.lat, self.lon, self.time)
+		val = 69 - self.alt(self.lat, self.lon, self.time)
 		left = tan(radians(val))
 		right = o_a_dist1
 		x = left * right
-		print("Calculated, effective actuator height: ", x, " inches")
+		print("Effective actuator1 height: ", x, " inches")
 		return x
 
 	"""Return the value calculated via the law of consines, the # of inches the second actuator must be move in order to pan the solar panel according to the azimuth."""
 	def calcPanningHeight(self, o_a_dist2):
 		azimuth = self.azimuth(self.lat, self.lon, self.time)
 		# if azimuth < 0:
-		# 	azimuth = 360 + azimuth
+		# 	azimuth = 360 + azimuth #TODO: unsure
 		val = 2*o_a_dist2*o_a_dist2 - (2*o_a_dist2*o_a_dist2*cos((radians(azimuth))))
 		x = math.sqrt(val)
-		print("value to sqrt: ", val)
-		print("Effective panning height(x): ", x)
-		print("azimuth: ", azimuth, " ", radians(azimuth))
+		#print("value to sqrt: ", val)
+		print("Effective actuator2 height: ", x)
 		return x	
 
-	def demoDay(self, lat, lon, dateTimeObj):
-		print()
-		try:
-			# print("Demo of ", self.name, " ", dateTimeObj)
-			# date = datetime.datetime(2015,5,15,6,55,0)
-			# print("lat, lon: ", lat, " ", lon)
-			# for h in range(0, 12):
-			# 	date += datetime.timedelta(hours=+1)
-			print("Demo of ", self.name, " ", dateTimeObj)
-			print("lat, lon: ", lat, " ", lon, "\n")
 
-			for h in range(0, 12):
-				self.incrementTime(t)
-				print(self.time)
-				print_alt(self)
-				print_actuator1(self)
+	"""Print the actuator values at hourly increments starting at input time."""
+	def demoDay(self, lat, lon, time):
+			print()
+			try:
+				#print("Demo of ", self.name, " ", time, ":")
 				print()
-
-			self.resetTime(original)
-		except ValueError:
-			self.resetTime(original)
-			pass
+				print("lat,lon: (", lat, ", ", lon, ") \n")
+				for h in range(0, 12):
+					print(self.time)
+					print_alt(self)
+					self.calcTiltingHeight(self.o_a_dist1)
+					self.incrementTime(self.time)
+					#print()
+				self.resetTime(now)
+			except ValueError:
+				self.resetTime(now)
+				pass
 
 """Print all relevant location data"""
 def printLocationInfo(loc):
@@ -122,28 +113,3 @@ def print_azimuth(loc):
 	print(str(loc.name) + " azimuth: ", loc.azimuth(loc.lat, loc.lon, loc.time))
 def print_actuator1(loc):
 	print(str(loc.name) + " actuator height: ", loc.calcTiltingHeight(loc.o_a_dist1))
-
-
-# """Calculate the most effective height of the first tilting actuator based upon the assumption solar panels are most efficent angled 45 degrees to the sun. Dist(a, o) is represented by base. Math: tan(S1) = a / distActuatorToOrigin & S1 = 90 - altitude ==> a = tan(90-altitude) * distActuatorToOrigin"""
-# def calcTiltingHeight(loc, o_a_dist1):
-# 	val = 90 - loc.alt(loc.lat, loc.lon)
-# 	left = tan(radians(val))
-# 	right = o_a_dist1
-# 	a = left * right
-# 	print("Calculated, effective actuator height: ", a, " inches")
-# 	return a
-
-#######################################################################################################################
-#New stuff 
-
-# """Return the value calculated via the law of consines, the # of inches the second actuator must be move in order to pan the solar panel according to the azimuth."""
-# def calcPanningHeight(loc, o_a_dist2):
-# 	azimuth = loc.azimuth(loc.lat, loc.lon)
-# 	# if azimuth < 0:
-# 	# 	azimuth = 360 + azimuth
-# 	val = 2*o_a_dist2*o_a_dist2 - (2*o_a_dist2*o_a_dist2*cos((radians(azimuth))))
-# 	x = math.sqrt(val)
-# 	print("value to sqrt: ", val)
-# 	print("Effective panning height(x): ", x)
-# 	print("azimuth: ", azimuth, " ", radians(azimuth))
-# 	return x
