366a62b12e8f46646b7e0474957bb03cba3dba0c
Ryan Kapur <ryankapur@berkeley.edu>
2015-07-07 19:23:47 -0700

    modified:   nodejs-proxy.js

--- python/nodejs-proxy.js
+++ python/nodejs-proxy.js
@@ -25,3 +25,20 @@ wss.on('connection', function(ws) {
     });
     ws.send('something');
 });
+
+///////////////////////////////////////////////////////
+
+
+
+//Sending and receiving text data
+var WebSocket = require('ws');
+var ws = new WebSocket('ws://www.host.com/path');
+
+ws.on('open', function open() {
+  ws.send('something');
+});
+
+ws.on('message', function(data, flags) {
+  // flags.binary will be set if a binary data is received.
+  // flags.masked will be set if the data was masked.
+});
