93b33ae1bc7bd2ff2382dd05222c1aa55b0dd5d1
ryankapur <ryankapur@berkeley.edu>
2015-07-21 16:28:50 -0700

    search for config.json in 3 specific locations

--- python/run_actuators.py
+++ python/run_actuators.py
@@ -3,10 +3,22 @@
 from icarus.actuator_pwm import Run
 from icarus.solarserver import MyServer
 from icarus.constants import Config
+from os.path import expanduser
 import json
 import signal
+import time
 
-x = Config("/etc/icarus/config.json")
+#import config.json
+try:
+	x = Config("config.json")
+
+except IOError:
+	try:
+		home = expanduser("~")
+		x = Config(str(home) + "/.config/icarus/config.json")
+	
+	except IOError:
+			x = Config("/etc/icarus/config.json")
 
 #instantiate websocketclient
 go = Run("127.0.0.1", "8080", x.maxActuatorHeight, x.speed)
